e90d2f4e6d80137200092eca09c0b28b58d2574e
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-03-17 16:07:59 -0700

    Cleaned up views. Changed datetime to utcnow.

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -25,7 +25,6 @@ Base = declarative_base()
 class Entry(Base):
     __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
-    # Try varchar here if it doens't work
     title = Column(Unicode(128), unique=True, nullable=False)
     text = Column(UnicodeText)
-    created = Column(DateTime, default=datetime.datetime.now())
+    created = Column(DateTime, default=datetime.datetime.utcnow)
--- learning_journal/views.py
+++ learning_journal/views.py
@@ -1,55 +1,20 @@
-from pyramid.response import Response
 from pyramid.view import view_config
-
-from sqlalchemy.exc import DBAPIError
-
 from .models import (
     DBSession,
     Entry,
-    )
-
-
-# @view_config(route_name='home', renderer='templates/mytemplate.pt')
-# def my_view(request):
-#     try:
-#         one = DBSession.query(Entry).filter(Entry.name == 'one').first()
-#     except DBAPIError:
-#         return Response(conn_err_msg, content_type='text/plain', status_int=500)
-#     return {'one': one, 'project': 'learning_journal'}
+)
 
 
 @view_config(route_name='home', renderer='templates/list.jinja2')
 def list(request):
-    try:
-        articles = DBSession.query(Entry).all()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+    """List view."""
+    articles = DBSession.query(Entry).all()
     return {'articles': articles}
-    # import pdb; pdb.set_trace()
-    # return 'Cheesecake is yummy!'
 
 
 @view_config(route_name='article', renderer='templates/detail.jinja2')
 def detail(request):
-    try:
-        article_id = '{article_id}'.format(**request.matchdict)
-        article = DBSession.query(Entry).filter(Entry.id == article_id).first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+    """Detail view."""
+    article_id = request.matchdict['article_id']
+    article = DBSession.query(Entry).get(article_id)
     return {'article': article}
-
-conn_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to run the "initialize_learning_journal_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
