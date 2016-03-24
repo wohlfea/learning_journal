3894b86a27d8be21e5669d70e55cc3f3e89da3d3
wohlfea <wohlfea@gmail.com>
2016-03-24 16:35:11 -0700

    fixed initializedb

--- learning_journal/scripts/initializedb.py
+++ learning_journal/scripts/initializedb.py
@@ -30,6 +30,9 @@ def initialize_db(argv=sys.argv):
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
     settings = get_appsettings(config_uri, options=options)
+    database_url = os.environ.get('DATABASE_URL', None)
+    if database_url is not None:
+        settings['sqlalchemy.url'] = database_url
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
